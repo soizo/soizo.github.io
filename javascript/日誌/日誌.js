8c794fcaa7606101132c6dc4d50ba12cf117e999
soizo <fileskillerdesu@gmail.com>
2024-03-21 07:35:07 +0800

    （高冷音）哦

---
+++ javascript/日誌/日誌.js
@@ -0,0 +1,15 @@
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   const images = document.querySelectorAll(".圖片全屏");
+//   images.forEach(function (img) {
+//     img.style.cursor = "pointer"; // 確保所有圖片都可以點擊
+//     img.addEventListener("click", function () {
+//       if (img.classList.contains("fullscreen")) {
+//         img.classList.remove("fullscreen");
+//         document.body.style.overflow = "auto"; // 還原時允許滾動
+//       } else {
+//         img.classList.add("fullscreen");
+//         document.body.style.overflow = "hidden"; // 放大時禁止背景滾動
+//       }
+//     });
+//   });
+// });
