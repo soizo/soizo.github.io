6088d5af1cf64feb5ec0fa2946d4788023ed4b1b
soizo <fileskillerdesu@gmail.com>
2024-03-23 22:41:49 +0800

    改豎排符號了

--- javascript/日誌/日誌.js
+++ javascript/日誌/日誌.js
@@ -1,15 +1,12 @@
 // document.addEventListener("DOMContentLoaded", (event) => {
 //   const images = document.querySelectorAll(".圖片全屏");
 //   images.forEach(function (img) {
-//     img.style.cursor = "pointer"; // 確保所有圖片都可以點擊
-//     img.addEventListener("click", function () {
+//     img.style.cursor = "pointer";//     img.addEventListener("click", function () {
 //       if (img.classList.contains("fullscreen")) {
 //         img.classList.remove("fullscreen");
-//         document.body.style.overflow = "auto"; // 還原時允許滾動
-//       } else {
+//         document.body.style.overflow = "auto";//       } else {
 //         img.classList.add("fullscreen");
-//         document.body.style.overflow = "hidden"; // 放大時禁止背景滾動
-//       }
+//         document.body.style.overflow = "hidden";//       }
 //     });
 //   });
 // });
