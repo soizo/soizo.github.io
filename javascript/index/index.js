d0d1bd738c9ceb4d833770b6064924f608af4ace
soizo <fileskillerdesu@gmail.com>
2024-03-26 21:48:45 +0800

    字體和index

--- javascript/index/index.js
+++ javascript/index/index.js
@@ -1,24 +1,22 @@
-// 初始化字典，使用十六進制碼點
 const 私用字辭典 = {
   即: ["卽"],
   今: [0xf0000],
   龜: [0xf0001],
-  臺: [0xf0002],
+  臺: [0xf0002, 0xf0004],
+  檯: [0xf0004],
   鬱: [0xf0003],
+  酷: [0x31406],
 };
 
-// 轉換字典中的碼點為實際字符
-const 是十六進制碼點 = (value) => typeof value === "number" && !isNaN(value);
-
-// 轉換字典中的碼點或保留原文本
-const 轉換字典 = Object.keys(私用字辭典).reduce((新字典, key) => {
-  新字典[key] = 私用字辭典[key].map((value) =>
-    是十六進制碼點(value) ? String.fromCodePoint(value) : value
-  );
-  return 新字典;
-}, {});
-
 document.addEventListener("DOMContentLoaded", () => {
+  const 是十六進制碼點 = (value) => typeof value === "number" && !isNaN(value);
+
+  const 轉換字典 = Object.keys(私用字辭典).reduce((新字典, key) => {
+    新字典[key] = 私用字辭典[key].map((value) =>
+      是十六進制碼點(value) ? String.fromCodePoint(value) : value
+    );
+    return 新字典;
+  }, {});
   HTML內私用字替換(document.body);
 
   const observer = new MutationObserver((mutations) => {
@@ -38,18 +36,51 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function HTML內私用字替換(node) {
     if (node.nodeType === 3) {
-      // 文本節點
       const regex = /\:\=([^\s\=]+?)\$(\d+)\=\:/g;
       node.data = node.data.replace(regex, (match, key, index) => {
         index = parseInt(index, 10);
         if (轉換字典.hasOwnProperty(key) && index < 轉換字典[key].length) {
           return 轉換字典[key][index];
         }
-        return match; // 如果沒找到，保留原始匹配
+        return match;
       });
     } else if (node.nodeType === 1) {
-      // 元素節點
       node.childNodes.forEach((childNode) => HTML內私用字替換(childNode));
     }
   }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const 遮擋元素們 = document.querySelectorAll(".遮擋下");
+
+  // 处理鼠标悬停事件
+  遮擋元素們.forEach((遮擋元素) => {
+    遮擋元素.classList.remove("hide-overlay");
+    遮擋元素.classList.add("hide-children");
+    遮擋元素.addEventListener("mouseenter", function () {
+      this.classList.add("hide-overlay");
+      this.classList.remove("hide-children");
+    });
+    遮擋元素.addEventListener("mouseleave", function () {
+      if (!window.getSelection().toString()) {
+        this.classList.remove("hide-overlay");
+        this.classList.add("hide-children");
+      }
+    });
+  });
+
+  // 处理文本选择变化事件
+  document.addEventListener("selectionchange", function () {
+    const selectedText = window.getSelection().toString();
+    遮擋元素們.forEach((遮擋元素) => {
+      // 检查当前遮挡元素或其子元素是否包含选中的文本
+      const isTextSelectedWithin =
+        selectedText &&
+        (遮擋元素.textContent.includes(selectedText) ||
+          selectedText.includes(遮擋元素.textContent));
+      // 添加或移除类以反映当前状态
+      遮擋元素.classList.toggle("hide-overlay", isTextSelectedWithin);
+      遮擋元素.classList.toggle("hide-children", !isTextSelectedWithin);
+    });
+  });
 });
