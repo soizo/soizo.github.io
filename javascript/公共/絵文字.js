ba9448da8153cb66888724fbd22e884cfb6b11ae
soizo <fileskillerdesu@gmail.com>
2024-03-27 20:12:28 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   "assets/\346\227\245\350\252\214.txt"
    	deleted:    "assets/\347\265\265\346\226\207\345\255\227/CHINA-FLAG.jpg"
    	deleted:    "assets/\347\265\265\346\226\207\345\255\227/CODE.jpg"
    	modified:   css/index.css
    	modified:   "css/\346\227\245\350\252\214.css"
    	modified:   index.html
    	modified:   "javascript/\345\205\254\345\205\261/\347\265\265\346\226\207\345\255\227.js"
    	modified:   "javascript/\346\227\245\350\252\214/\345\207\246\347\220\206\346\226\207\346\234\254.js"
    	modified:   "\346\227\245\350\252\214.html"
    
    Changes not staged for commit:
    	modified:   "assets/\346\227\245\350\252\214.txt"

--- javascript/公共/絵文字.js
+++ javascript/公共/絵文字.js
@@ -1,40 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // 在文檔加載完成後，立即運行一次對整個文檔體的處理
   replaceTextWithImages(document.body);
-  function replaceTextWithImages(node) {
-    if (node.nodeType === 3) {
-      // 處理純文本節點
-      replaceTextInNode(node);
-    } else if (
-      node.nodeType === 1 &&
+});
+
+// 將replaceTextWithImages函數暴露到全局作用域
+window.絵文字替換 = replaceTextWithImages;
+
+function replaceTextWithImages(node) {
+  if (node.nodeType === 3) {
+    // 處理文本節點
+    replaceTextInTextNode(node);
+  } else if (node.nodeType === 1) {
+    // 處理元素節點
+    if (
       node.tagName.toUpperCase() !== "SCRIPT" &&
       node.tagName.toUpperCase() !== "STYLE"
     ) {
-      if (node.hasChildNodes()) {
-        // 遞歸處理所有子節點
-        Array.from(node.childNodes).forEach(replaceTextWithImages);
+      if (node.childNodes.length === 1 && node.childNodes[0].nodeType === 3) {
+        replaceTextInTextNode(node.childNodes[0]);
       } else {
-        // 處理包含HTML的文本
-        replaceTextInNode(node);
+        Array.from(node.childNodes).forEach((childNode) =>
+          replaceTextWithImages(childNode)
+        );
       }
     }
   }
+}
 
-  function replaceTextInNode(node) {
-    const regex = /\:\=\&(.+?)\=\:/g;
-    let textContent = node.nodeType === 3 ? node.data : node.innerHTML;
-    const modifiedText = textContent.replace(regex, (match, imageName) => {
-      const imgSrc = `/assets/絵文字/${imageName}.gif`;
-      return `<img src="${imgSrc}" class="符號圖象" draggable="false">`;
-    });
+function replaceTextInTextNode(textNode) {
+  const regex = /\:\=\&(.+?)\=\:/g; // 匹配絵文字代碼的正則表達式
+  let modifiedText = textNode.data.replace(regex, (match, imageName) => {
+    const imgSrc = `/assets/絵文字/${imageName}.gif`; // 組裝圖片的源地址
+    return `<img src="${imgSrc}" class="絵文字圖象" draggable="false">`; // 返回替換文本的HTML
+  });
 
-    if (modifiedText !== textContent) {
-      if (node.nodeType === 3) {
-        const wrapper = document.createElement("span");
-        wrapper.innerHTML = modifiedText;
-        node.parentNode.replaceChild(wrapper, node);
-      } else {
-        node.innerHTML = modifiedText;
-      }
-    }
+  if (modifiedText !== textNode.data) {
+    const wrapper = document.createElement("span");
+    wrapper.innerHTML = modifiedText;
+    textNode.parentNode.replaceChild(wrapper, textNode);
   }
-});
+}
